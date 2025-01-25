c3bb59c5f6e2a4a5e1e1006cf09ddbbf1abec467
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-25 16:32:36 +0000

    feat(domain): aprimora entidade Categoria
    - Adiciona validações com DataAnnotations
    - Configura propriedades com valores padrão
    - Inicializa coleção de Produtos no construtor
    - Implementa ICollection para melhor abstração
    - Define tamanhos máximos para campos

--- src/PrateleiraInteligente.Domain/Entities/Categoria.cs
+++ src/PrateleiraInteligente.Domain/Entities/Categoria.cs
@@ -1,10 +1,24 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace PrateleiraInteligente.Domain.Entities
 {
     public class Categoria
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Descricao { get; set; }
-        public List<Produto> Produtos { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; } = string.Empty;
+
+        [StringLength(500)]
+        public string Descricao { get; set; } = string.Empty;
+
+        public virtual ICollection<Produto> Produtos { get; set; }
+
+        public Categoria()
+        {
+            Produtos = new List<Produto>();
+        }
     }
 }
