8164d9dae6b76c1e7c07183f9aa555a684126f30
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-25 17:03:05 +0000

    feat(domain): aprimora entidade Movimentacao
    - Adiciona construtor com valores iniciais
    - Configura navegação virtual para Produto
    - Adiciona validação de quantidade mínima
    - Inclui DataAnnotations para validações
    - Define tamanho máximo para Observacao

--- src/PrateleiraInteligente.Domain/Entities/Movimentacao.cs
+++ src/PrateleiraInteligente.Domain/Entities/Movimentacao.cs
@@ -1,13 +1,28 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace PrateleiraInteligente.Domain.Entities
 {
     public class Movimentacao
     {
+        public Movimentacao()
+        {
+            Observacao = string.Empty;
+            DataMovimentacao = DateTime.Now;
+        }
+
         public int Id { get; set; }
         public int ProdutoId { get; set; }
-        public Produto Produto { get; set; }
+        public virtual Produto Produto { get; set; } = null!;
         public TipoMovimentacao Tipo { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "A quantidade deve ser maior que zero")]
         public int Quantidade { get; set; }
+
         public DateTime DataMovimentacao { get; set; }
+
+        [StringLength(500)]
         public string Observacao { get; set; }
     }
 
